f866939393dd98406b7f8c6604272674d0760210
LcYxT <lcytimothy@gmail.com>
2023-06-09 19:12:53 +0800

    change video

--- app/test-video/page.tsx
+++ app/test-video/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.scss'
 function Page() {
   return (<>
     <div className={styles["container-wrap"]} aria-controls=''>
-      <video height="100%" width='100%' controls><source src="https://www.googleapis.com/drive/v3/files/17hcRvLoksZ6yvI9KqizfBhpWSHSaW7uR?key=***REMOVED***&alt=media" /></video>
+      <video height="100%" width='100%' controls><source src="https://www.googleapis.com/drive/v3/files/1I745TlKjoRW0tOFuvwI-26K3STKl0rwJ?key=***REMOVED***" /></video>
     </div>
   </>
   )
