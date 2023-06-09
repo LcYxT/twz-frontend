6708819f3ab8d66181c817bf15f82ca0c8936eff
LcYxT <lcytimothy@gmail.com>
2023-06-09 19:14:26 +0800

    alt media

--- app/test-video/page.tsx
+++ app/test-video/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.scss'
 function Page() {
   return (<>
     <div className={styles["container-wrap"]} aria-controls=''>
-      <video height="100%" width='100%' controls><source src="https://www.googleapis.com/drive/v3/files/1I745TlKjoRW0tOFuvwI-26K3STKl0rwJ?key=***REMOVED***" /></video>
+      <video height="100%" width='100%' controls><source src="https://www.googleapis.com/drive/v3/files/1I745TlKjoRW0tOFuvwI-26K3STKl0rwJ?key=***REMOVED***&alt=media" /></video>
     </div>
   </>
   )
