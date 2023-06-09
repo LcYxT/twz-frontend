840cd9886a39172313953d1b4dd23719219db454
LcYxT <lcytimothy@gmail.com>
2023-06-09 19:42:26 +0800

    change url

--- app/test-video/page.tsx
+++ app/test-video/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.scss'
 function Page() {
   return (<>
     <div className={styles["container-wrap"]} aria-controls=''>
-      <video height="100%" width='100%' controls><source src="https://www.googleapis.com/drive/v3/files/1I745TlKjoRW0tOFuvwI-26K3STKl0rwJ?key=***REMOVED***&alt=media" /></video>
+      <video height="100%" width='100%' controls><source src="https://files.mybox.naver.com/file/download.api?auth=3&resourceKey=YWlkZW5rb3J8MzQ3MjUwMDYzMTM1NjA3NjM1M3xGfDU5Njk1OTg=&atoken=***REMOVED***&NdriveSvcType=NHN%2FND-WEB%20Ver&resourceType=vencode" /></video>
     </div>
   </>
   )
